10b92f840bdb3a0df1a078e44e0f361d1fba1059
Vibhour <87240979+vibhourjain@users.noreply.github.com>
2025-03-01 19:59:23 +0800

    Create r.py
    r.py

---
+++ r.py
@@ -0,0 +1,3 @@
+<a href="mailto:manager@company.com?subject=Authorization%20Request&body=%3C%21DOCTYPE%20HTML%3E%0D%0A%3Chtml%3E%0D%0A%3Cbody%3E%0D%0A%3Ch1%3EApproval%20Request%3C%2Fh1%3E%0D%0A%3Cp%3EHostname%3A%20%3Cb%3Eserver-01%3C%2Fb%3E%3C%2Fp%3E%0D%0A%3Ca%20href%3D%22https%3A%2F%2Fapprove.example.com%3Fhost%3Dserver01%26user%3Dadmin%22%3EClick%20to%20Approve%3C%2Fa%3E%0D%0A%3C%2Fbody%3E%0D%0A%3C%2Fhtml%3E">
+  Request Approval via Outlook
+</a>
