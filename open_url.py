964e7403a2794cc083bb9d316b6d498d5bc03f4a
vibhourjain <vibhourjan@gmail.com>
2025-05-02 13:07:26 +0800

    ADO-0001

--- open_url.py
+++ open_url.py
@@ -1,21 +1,20 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
-# Title
-st.title("Ticket Search")
+st.title("Open Ticket in New Tab")
 
-# Ticket input
 ticket_number = st.text_input("Enter Ticket Number (e.g., WO123455):")
 
-# Submit button
 if st.button("Open Ticket"):
     if ticket_number:
         url = f"https://www.vibhour.com/apse/search/{ticket_number}"
-        # JavaScript to open in new tab
-        js = f"""
-        <script>
-        window.open("{url}", "_blank").focus();
-        </script>
-        """
-        st.markdown(js, unsafe_allow_html=True)
+        components.html(
+            f"""
+            <script>
+            window.open("{url}", "_blank");
+            </script>
+            """,
+            height=0,
+        )
     else:
         st.warning("Please enter a valid ticket number.")
